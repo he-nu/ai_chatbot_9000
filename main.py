ac78d44e11efa7ee9f8fd14fb59b2743502f774f
HenriN MSI <henri.nuortila@gmail.com>
2023-11-09 15:16:32 +0200

    changed the data structure and main structure

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
 #! /usr/bin/python3.10
+import json
+import re
+
+# KEYWORDS:list = ...
+
+
+def get_questions(file_path:str)-> dict:
+    ...
+
+
+def ask_user_input():
+    user_input = input("What would you like to know about AI?")
+    return user_input
+
+
+
+def find_similarities(keywords_list:list, user_input:str) -> list:
+    """ Uses regular expressions to find similarities. 
+    Returns up to 3 closest hits."""
+    ...
+
+def main():
+    questions = get_questions()
+    user_input = ask_user_input()
+    similarities = find_similarities()
+
+
 
